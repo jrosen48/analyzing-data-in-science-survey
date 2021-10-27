56973bb68ef428d6ec9ee93b020d4e8efc976cd4
Joshua Rosenberg <jmichaelrosenberg@gmail.com>
2021-10-27 11:28:08 -0400

    update targts

--- _targets.R
+++ _targets.R
@@ -5,15 +5,15 @@ tar_option_set(packages = c("tidyverse", "here", "janitor"))
 
 # End this file with a list of target objects.
 list(
-  tar_target(data_mailing_list, read_csv(here("data", "Analyzing+Data+in+Science_February+14,+2021_10.51.csv"))),
+  tar_target(data_mailing_list, read_csv(here("data", "analyzing-data-in-science-mailing-lists-response-options.csv"))),
   tar_target(data_mailing_list_cleaned_names, janitor::clean_names(data_mailing_list)),
   tar_target(data_mailing_list_sliced, slice(data_mailing_list_cleaned_names, -c(1, 2))),
   tar_target(data_mailing_list_processed, mutate(data_mailing_list_sliced, collection = "mailing-list")),
   
-  tar_target(data_social_media, read_csv(here("data", "Analyzing+Data+in+Science+-+Copy+for+Mailing+List+Posts_February+14,+2021_10.50.csv"))),
+  tar_target(data_social_media, read_csv(here("data", "analyzing-data-in-science-social-media-response-options.csv"))),
   tar_target(data_social_media_cleaned_names, janitor::clean_names(data_social_media)),
   tar_target(data_social_media_sliced, slice(data_social_media_cleaned_names, -c(1, 2))),
-  tar_target(data_social_media_processed, mutate(data_social_media_sliced, collection = "mailing-list")),
+  tar_target(data_social_media_processed, mutate(data_social_media_sliced, collection = "social-media")),
   
   tar_target(data_combined, bind_rows(data_mailing_list_processed, data_social_media_processed)),
   
